7131eea548d0e35bba402ba1fd92a598978a9b48
Zack <stonezack633@gmail.com>
2024-03-05 13:31:08 -0600

    Started report page

---
+++ app/src/main/java/com/example/idlereasonsproject/ReportIdle.java
@@ -0,0 +1,8 @@
+package com.example.idlereasonsproject;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public abstract class ReportIdle extends Context {
+}
