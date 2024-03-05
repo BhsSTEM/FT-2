8794692493c50085937b3eee3ac27af7adf4b9c7
Zack <stonezack633@gmail.com>
2024-03-05 15:22:54 -0600

    Finished (hopfully) design of report page, started coding it. Also added a to do list in the read me for me to use

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # FT-2
-# Zack S
+#
+# Needed:
+# Ability to actually get to the report idle screen
+# Way to take in information from the text box
+# Creation of like, a report object? That seems like a reasonable way to go about this
+# Ability to get all the information into said report object
+# And maybe way in the future a way to grab the options from a database instead of a set string
--- app/src/main/java/com/example/idlereasonsproject/ReportIdle.java
+++ app/src/main/java/com/example/idlereasonsproject/ReportIdle.java
@@ -1,8 +1,64 @@
 package com.example.idlereasonsproject;
 
-import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.AdapterView.OnItemSelectedListener;
 
-public abstract class ReportIdle extends Context {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ReportIdle extends AppCompatActivity implements OnItemSelectedListener{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+// Spinner for fields
+        Spinner fieldSpinner = (Spinner) findViewById(R.id.report_idle_field);
+        // Create an ArrayAdapter using the string array and a default spinner layout.
+        ArrayAdapter<CharSequence> fieldAdapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.fields_array,
+                android.R.layout.simple_spinner_item
+        );
+        // Specify the layout to use when the list of choices appears.
+        fieldAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner.
+        fieldSpinner.setAdapter(fieldAdapter);
+// Spinner for machines
+        Spinner machineSpinner = (Spinner) findViewById(R.id.report_idle_machine);
+        // Create an ArrayAdapter using the string array and a default spinner layout.
+        ArrayAdapter<CharSequence> machineAdapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.machines_array,
+                android.R.layout.simple_spinner_item
+        );
+        // Specify the layout to use when the list of choices appears.
+        machineAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner.
+        machineSpinner.setAdapter(machineAdapter);
+// Spinner for reasons
+        Spinner reasonSpinner = (Spinner) findViewById(R.id.report_idle_reason);
+        // Create an ArrayAdapter using the string array and a default spinner layout.
+        ArrayAdapter<CharSequence> reasonAdapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.reasons_array,
+                android.R.layout.simple_spinner_item
+        );
+        // Specify the layout to use when the list of choices appears.
+        reasonAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner.
+        reasonSpinner.setAdapter(reasonAdapter);
+    }
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item is selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos).
+        String test = (String) parent.getItemAtPosition(pos);
+        System.out.println(test);
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback.
+    }
 }
