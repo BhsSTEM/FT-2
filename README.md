e55e31e26839ea06ecc7ece17d760aacdeea1092
Zack <stonezack633@gmail.com>
2024-03-18 15:22:32 -0500

    Redoing/fixing report java

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # FT-2
 #
 # Needed:
-# The text saying "fields" "machines" etc are editable which they shouldn't be
 # Way to take in information from the text box
 # Creation of like, a report object? That seems like a reasonable way to go about this
 # Ability to get all the information into said report object
-# And maybe way in the future a way to grab the options from a database instead of a set string
+# And maybe way in the future a way to grab the options from a database instead of a set string array
--- app/src/main/java/com/example/idlereasonsproject/ReportIdle.java
+++ app/src/main/java/com/example/idlereasonsproject/ReportIdle.java
@@ -1,6 +1,7 @@
 package com.example.idlereasonsproject;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -10,13 +11,14 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ReportIdle extends AppCompatActivity implements OnItemSelectedListener{
+public class ReportIdle extends AppCompatActivity implements OnItemSelectedListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.report_idle);
-// Spinner for fields
-        Spinner fieldSpinner = (Spinner) findViewById(R.id.report_idle_field);
+
+//Field Spinner
+        Spinner fieldSpinner = findViewById(R.id.report_idle_field);
         // Create an ArrayAdapter using the string array and a default spinner layout.
         ArrayAdapter<CharSequence> fieldAdapter = ArrayAdapter.createFromResource(
                 this,
@@ -27,8 +29,11 @@ public class ReportIdle extends AppCompatActivity implements OnItemSelectedListe
         fieldAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner.
         fieldSpinner.setAdapter(fieldAdapter);
-// Spinner for machines
-        Spinner machineSpinner = (Spinner) findViewById(R.id.report_idle_machine);
+        fieldSpinner.setOnItemSelectedListener(this);
+        Log.i("My Tag", "Field spinner up");
+
+//Machine Spinner
+        Spinner machineSpinner = findViewById(R.id.report_idle_machine);
         // Create an ArrayAdapter using the string array and a default spinner layout.
         ArrayAdapter<CharSequence> machineAdapter = ArrayAdapter.createFromResource(
                 this,
@@ -39,8 +44,10 @@ public class ReportIdle extends AppCompatActivity implements OnItemSelectedListe
         machineAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner.
         machineSpinner.setAdapter(machineAdapter);
-// Spinner for reasons
-        Spinner reasonSpinner = (Spinner) findViewById(R.id.report_idle_reason);
+        machineSpinner.setOnItemSelectedListener(this);
+        Log.i("My Tag", "Machine spinner up");
+//Reason Spinner
+        Spinner reasonSpinner = findViewById(R.id.report_idle_reason);
         // Create an ArrayAdapter using the string array and a default spinner layout.
         ArrayAdapter<CharSequence> reasonAdapter = ArrayAdapter.createFromResource(
                 this,
@@ -51,11 +58,22 @@ public class ReportIdle extends AppCompatActivity implements OnItemSelectedListe
         reasonAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner.
         reasonSpinner.setAdapter(reasonAdapter);
-}
+        reasonSpinner.setOnItemSelectedListener(this);
+        Log.i("My Tag", "Reason spinner up");
+
+//Variable set up
+        String location = "Unknown";
+        String machine = "Unknown";
+        String reason = "Unknown";
+        String furtherInformation = "Blank";
+        Log.i("My Tag", "Variables set up");
+    }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
+        String result = (String) parent.getItemAtPosition(position);
+        Log.i("My Tag", result);
+        //Best way to figure out which variables to change is probably a switch statment
     }
 
     @Override
