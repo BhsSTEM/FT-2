acce278365f54ebaead43d7e7c670a11364a4ce5
Zack <stonezack633@gmail.com>
2024-03-19 15:24:27 -0500

    Adding time of report to the ReportObject

--- app/src/main/java/com/example/idlereasonsproject/ReportObject.java
+++ app/src/main/java/com/example/idlereasonsproject/ReportObject.java
@@ -1,17 +1,21 @@
 package com.example.idlereasonsproject;
 
 import android.util.Log;
+import java.util.Calendar;
+import java.util.Date;
 
 public class ReportObject {
     String location = "Unknown";
     String machine = "Unknown";
     String reason = "Unknown";
     String furtherInformation = "Blank";
+    Date timeOfSubmission;
     public void assignData(String newLocation, String newMachine, String newReason, String newFurtherInformation){
         location = newLocation;
         machine = newMachine;
         reason = newReason;
         furtherInformation = newFurtherInformation;
+        timeOfSubmission = Calendar.getInstance().getTime();
         Log.i("Report object", "Data assigned");
     }
     public void reportToLogCat() {
@@ -19,5 +23,6 @@ public class ReportObject {
         Log.i("Submission", "Machine: " + machine);
         Log.i("Submission", "Reason: " + reason);
         Log.i("Submission", "Further Information: " + furtherInformation);
+        Log.i("Submission", "Time of submission: " + timeOfSubmission.toString());
     }
 }
