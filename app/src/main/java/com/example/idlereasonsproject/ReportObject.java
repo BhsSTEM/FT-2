dcc66323f9348103bfbefca727e899f94cbb630e
Zack <stonezack633@gmail.com>
2024-03-25 15:21:29 -0500

    Prepping ReportObject for being added to the database with some commented out functions

--- app/src/main/java/com/example/idlereasonsproject/ReportObject.java
+++ app/src/main/java/com/example/idlereasonsproject/ReportObject.java
@@ -25,17 +25,29 @@ public class ReportObject {
         Log.i("Submission", "Further Information: " + furtherInformation);
         Log.i("Submission", "Time of submission: " + timeOfSubmission.toString());
     }
+    //these types of functions seem to be standard and it can't hurt
+    public String getLocation() {return location;}
+    public String getMachine(){return machine;}
+    public String getReason(){return reason;}
+    public String getFurtherInformation(){return furtherInformation;}
+    public Date getTimeOfSubmission(){return timeOfSubmission;}
+    public String getTimeOfSubmissionAsString(){return timeOfSubmission.toString();}
+
     //For adding to database, should work when uncommented, maybe this file will need to be placed into FBDatabase I'm not sure
     //import java.util.HashMap;
     //import java.util.Map;
     //Place those with the other imports
-    /*public ReportObject(){} // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    /*public ReportObject(){} // Default constructor required for calls to DataSnapshot.getValue(User.class)*/
+    // this might need to go over public class ReportObject {
+    /*
     public Map<String, Object> toMap()
     {
         HashMap<String, Object> map  = new HashMap<>();
         map.put("location", location);
         map.put("machine", machine);
         map.put("reason", reason);
+        map.put("furtherInformation", furtherInformation);
+        map.put("timeOfSubmission", Calendar.getInstance().getTime());
 
         return map;
     }*/
