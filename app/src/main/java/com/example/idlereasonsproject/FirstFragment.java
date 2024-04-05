8b01d4f21735f372ca41a7b475b8f5c127902488
weatherspoonjackson <jack.weatherspoon7@gmail.com>
2024-04-05 15:20:06 -0500

    login complete

--- app/src/main/java/com/example/idlereasonsproject/FirstFragment.java
+++ app/src/main/java/com/example/idlereasonsproject/FirstFragment.java
@@ -1,6 +1,7 @@
 package com.example.idlereasonsproject;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -62,7 +63,9 @@ public class FirstFragment extends Fragment {
 
                 if(loginStatus)
                 {
-                    //login :)
+                    Log.i("login","complete");
+                    NavHostFragment.findNavController(FirstFragment.this)
+                            .navigate(R.id.action_FirstFragment_to_HomeFragment);
                 }
                 else
                 {
