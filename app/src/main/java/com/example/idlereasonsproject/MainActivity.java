a0c29cedeb1c7f6758e541f0a80a85482aef23bd
nikkiN007 <80715958+nikkiN007@users.noreply.github.com>
2024-04-09 13:41:09 -0500

    changes

--- app/src/main/java/com/example/idlereasonsproject/MainActivity.java
+++ app/src/main/java/com/example/idlereasonsproject/MainActivity.java
@@ -49,6 +49,13 @@ public class MainActivity extends AppCompatActivity {
 
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
+        String[] mList = new String[] {
+                "1",  "2",  "3",  "4",  "5",  "6",  "7",  "8",
+                "9",  "10", "11", "12", "13", "14", "15", "16",
+                "17", "18", "19", "20", "21", "22", "23", "24",
+                "25", "26", "27", "28", "29", "30"
+        };
+
 
 /*
         binding.fab.setOnClickListener(new View.OnClickListener() {
