9c9258de3c6776916c1445ae870bbbff35d8e3ff
Zack <stonezack633@gmail.com>
2024-04-11 14:25:53 -0500

    Reverting back merge commit

--- app/src/main/java/com/example/idlereasonsproject/AppStartUp.java
+++ app/src/main/java/com/example/idlereasonsproject/AppStartUp.java
@@ -3,9 +3,11 @@ package com.example.idlereasonsproject;
 import android.app.Application;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.content.Context;
 import android.os.Build;
 import android.util.Log;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class AppStartUp extends Application {
     public void MyApp() {
@@ -41,5 +43,55 @@ public class AppStartUp extends Application {
             notificationManager.createNotificationChannel(channel);
             Log.i("Notifications", "Channel '" + name + "' created");
         }
+    }
+
+    public static class ReportObject {
+        String location = "Unknown";
+        String machine = "Unknown";
+        String reason = "Unknown";
+        String furtherInformation = "Blank";
+        Date timeOfSubmission;
+        public void assignData(String newLocation, String newMachine, String newReason, String newFurtherInformation){
+            location = newLocation;
+            machine = newMachine;
+            reason = newReason;
+            furtherInformation = newFurtherInformation;
+            timeOfSubmission = Calendar.getInstance().getTime();
+            Log.i("Report object", "Data assigned");
+        }
+        public void reportToLogCat() {
+            Log.i("Submission ", "Location: " + location);
+            Log.i("Submission", "Machine: " + machine);
+            Log.i("Submission", "Reason: " + reason);
+            Log.i("Submission", "Further Information: " + furtherInformation);
+            Log.i("Submission", "Time of submission: " + timeOfSubmission.toString());
+        }
+
+        //these types of functions seem to be standard and it can't hurt
+        public String getLocation() {return location;}
+        public String getMachine(){return machine;}
+        public String getReason(){return reason;}
+        public String getFurtherInformation(){return furtherInformation;}
+        public Date getTimeOfSubmission(){return timeOfSubmission;}
+        public String getTimeOfSubmissionAsString(){return timeOfSubmission.toString();}
+
+        //For adding to database, should work when uncommented, maybe this file will need to be placed into FBDatabase I'm not sure
+        //import java.util.HashMap;
+        //import java.util.Map;
+        //Place those with the other imports
+        /*public ReportObject(){} // Default constructor required for calls to DataSnapshot.getValue(User.class)*/
+        // this might need to go over public class ReportObject {
+        /*
+        public Map<String, Object> toMap()
+        {
+            HashMap<String, Object> map  = new HashMap<>();
+            map.put("location", location);
+            map.put("machine", machine);
+            map.put("reason", reason);
+            map.put("furtherInformation", furtherInformation);
+            map.put("timeOfSubmission", Calendar.getInstance().getTime());
+
+            return map;
+        }*/
     }
 }
