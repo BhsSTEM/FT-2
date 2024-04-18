d33b5ebd8542bd9ad10108094f0597e9fd42da1d
Zack <stonezack633@gmail.com>
2024-04-18 15:01:03 -0500

    User objects now have a boolean which says if they're an operator or not, defaults to false

--- app/src/main/java/com/example/idlereasonsproject/FBDatabase/ReportNode.java
+++ app/src/main/java/com/example/idlereasonsproject/FBDatabase/ReportNode.java
@@ -4,6 +4,7 @@ import android.util.Log;
 
 import androidx.annotation.NonNull;
 
+import com.example.idlereasonsproject.slothfulNotifications;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
--- app/src/main/java/com/example/idlereasonsproject/FBDatabase/User.java
+++ app/src/main/java/com/example/idlereasonsproject/FBDatabase/User.java
@@ -8,6 +8,7 @@ public class User
     private String firstName;
     private String lastName;
     private String password;
+    private boolean isOperator = false;
 
     public User(){} // Default constructor required for calls to DataSnapshot.getValue(User.class)
     public User(String _firstName, String _lastName, String _password)
@@ -24,22 +25,13 @@ public class User
         map.put("firstName", firstName);
         map.put("lastName", lastName);
         map.put("password", password);
+        map.put("isOperator", isOperator);
 
         return map;
     }
 
-    public String getFirstName()
-    {
-        return firstName;
-    }
-
-    public String getLastName()
-    {
-        return lastName;
-    }
-
-    public String getPassword()
-    {
-        return password;
-    }
+    public String getFirstName() {return firstName;}
+    public String getLastName() {return lastName;}
+    public String getPassword() {return password;}
+    public boolean isOperator() {return isOperator;}
 }
