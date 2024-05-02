448ed9cd790d41166b7aeda3550085eb6fcd53c2
Zack <stonezack633@gmail.com>
2024-05-02 14:52:43 -0500

    Minor cleanup of reportAnalysis

--- app/src/main/java/com/example/idlereasonsproject/reportAnalysis.java
+++ app/src/main/java/com/example/idlereasonsproject/reportAnalysis.java
@@ -1,18 +1,12 @@
 package com.example.idlereasonsproject;
 
 import android.util.Log;
-import android.widget.Switch;
-
-import com.example.idlereasonsproject.FBDatabase.MachineObject;
 import com.example.idlereasonsproject.FBDatabase.ReportNode;
 import com.example.idlereasonsproject.FBDatabase.ReportObject;
-import com.google.firebase.database.core.Repo;
-
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
-
 public class reportAnalysis {
+//Logging
     public static void reportObjectHashmapToLogCat(Map<String, ReportObject> map) {
         for (Map.Entry<String, ReportObject> entry : map.entrySet()) {
             String key = entry.getKey();
@@ -23,70 +17,7 @@ public class reportAnalysis {
     public static void reportMapReportTextToLogCat() {
         reportObjectHashmapToLogCat(ReportNode.getReportMap());
     }
-    public static int numOfReportsFromReporter(String reporter, Map<String, ReportObject> map) {
-        int numOfReports = 0;
-        for (Map.Entry<String, ReportObject> entry : map.entrySet()) {
-            String valueOfReporter = entry.getValue().getReporter();
-            if (reporter.equals(valueOfReporter)){
-                numOfReports++;
-            }
-        }
-        return numOfReports;
-    }
-    public static Map<String, ReportObject> reportsFromReporter(String reporter, Map<String, ReportObject> map) {
-        Map<String, ReportObject> returnedMap = new HashMap<>();
-        for (Map.Entry<String, ReportObject> entry : map.entrySet()) {
-            String key = entry.getKey();
-            ReportObject value = entry.getValue();
-            if (reporter.equals(value.getReporter())){
-                returnedMap.put(key, value);
-            }
-        }
-        return returnedMap;
-    }
-    public static int numOfReportsFromMachine(String machine, Map<String, ReportObject> map) {
-        int numOfReports = 0;
-        for (Map.Entry<String, ReportObject> entry : map.entrySet()) {
-            String valueOfMachine = entry.getValue().getMachine();
-            if (machine.equals(valueOfMachine)){
-                numOfReports++;
-            }
-        }
-        return numOfReports;
-    }
-    public static Map<String, ReportObject> reportsFromMachine(String machine, Map<String, ReportObject> map) {
-        Map<String, ReportObject> returnedMap = new HashMap<>();
-        for (Map.Entry<String, ReportObject> entry : map.entrySet()) {
-            String key = entry.getKey();
-            ReportObject value = entry.getValue();
-            if (machine.equals(value.getMachine())){
-                returnedMap.put(key, value);
-            }
-        }
-        return returnedMap;
-    }
-    public static int numOfUnresolvedReports(Map<String, ReportObject> map) {
-        int numOfReports = 0;
-        for (Map.Entry<String, ReportObject> entry : map.entrySet()) {
-            if (!entry.getValue().isResolved()) {
-                numOfReports++;
-            }
-            Log.i("numOfUnresolvedReports", String.valueOf(numOfReports));
-        }
-        return numOfReports;
-    }
-    public static Map<String, ReportObject> getUnresolvedReports(Map<String, ReportObject> map) {
-        Map<String, ReportObject> returnedMap = new HashMap<>();
-        for (Map.Entry<String, ReportObject> entry : map.entrySet()) {
-            String key = entry.getKey();
-            ReportObject value = entry.getValue();
-            if (!entry.getValue().isResolved()){
-                returnedMap.put(key, value);
-            }
-        }
-        return returnedMap;
-    }
-    public static long timeBetweenReportAndResolution(ReportObject report) {return report.getTimeOfResolution().getTime() - report.getTimeOfSubmission().getTime();}
+//Time related functions
     public static String idleLength (long lengthValue, boolean resolvedOrNot, String machine) {
         lengthValue = lengthValue - (lengthValue % 1000);
         long numOfSeconds = lengthValue/1000;
@@ -123,5 +54,74 @@ public class reportAnalysis {
             }
         }
         return returnedString;
+    }
+    public static long timeBetweenReportAndResolution(ReportObject report) {
+        return report.getTimeOfResolution().getTime() - report.getTimeOfSubmission().getTime();
+    }
+//Reports from a reporter
+    public static int numOfReportsFromReporter(String reporter, Map<String, ReportObject> map) {
+        int numOfReports = 0;
+        for (Map.Entry<String, ReportObject> entry : map.entrySet()) {
+            String valueOfReporter = entry.getValue().getReporter();
+            if (reporter.equals(valueOfReporter)){
+                numOfReports++;
+            }
+        }
+        return numOfReports;
+    }
+    public static Map<String, ReportObject> reportsFromReporter(String reporter, Map<String, ReportObject> map) {
+        Map<String, ReportObject> returnedMap = new HashMap<>();
+        for (Map.Entry<String, ReportObject> entry : map.entrySet()) {
+            String key = entry.getKey();
+            ReportObject value = entry.getValue();
+            if (reporter.equals(value.getReporter())){
+                returnedMap.put(key, value);
+            }
+        }
+        return returnedMap;
+    }
+//Reports from a machine
+    public static int numOfReportsFromMachine(String machine, Map<String, ReportObject> map) {
+        int numOfReports = 0;
+        for (Map.Entry<String, ReportObject> entry : map.entrySet()) {
+            String valueOfMachine = entry.getValue().getMachine();
+            if (machine.equals(valueOfMachine)){
+                numOfReports++;
+            }
+        }
+        return numOfReports;
+    }
+    public static Map<String, ReportObject> reportsFromMachine(String machine, Map<String, ReportObject> map) {
+        Map<String, ReportObject> returnedMap = new HashMap<>();
+        for (Map.Entry<String, ReportObject> entry : map.entrySet()) {
+            String key = entry.getKey();
+            ReportObject value = entry.getValue();
+            if (machine.equals(value.getMachine())){
+                returnedMap.put(key, value);
+            }
+        }
+        return returnedMap;
+    }
+//Unresolved reports
+    public static int numOfUnresolvedReports(Map<String, ReportObject> map) {
+        int numOfReports = 0;
+        for (Map.Entry<String, ReportObject> entry : map.entrySet()) {
+            if (!entry.getValue().isResolved()) {
+                numOfReports++;
+            }
+            Log.i("numOfUnresolvedReports", String.valueOf(numOfReports));
+        }
+        return numOfReports;
+    }
+    public static Map<String, ReportObject> getUnresolvedReports(Map<String, ReportObject> map) {
+        Map<String, ReportObject> returnedMap = new HashMap<>();
+        for (Map.Entry<String, ReportObject> entry : map.entrySet()) {
+            String key = entry.getKey();
+            ReportObject value = entry.getValue();
+            if (!entry.getValue().isResolved()){
+                returnedMap.put(key, value);
+            }
+        }
+        return returnedMap;
     }
 }
