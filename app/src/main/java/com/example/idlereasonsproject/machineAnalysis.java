5b296b3bca72bd86ed59c4b5f4c83634ad907572
Zack <stonezack633@gmail.com>
2024-05-02 14:42:01 -0500

    function to get an unresolved idle report which a machine is involved in

--- app/src/main/java/com/example/idlereasonsproject/machineAnalysis.java
+++ app/src/main/java/com/example/idlereasonsproject/machineAnalysis.java
@@ -1,5 +1,7 @@
 package com.example.idlereasonsproject;
 
+import android.util.Log;
+
 import com.example.idlereasonsproject.FBDatabase.MachineObject;
 import com.example.idlereasonsproject.FBDatabase.ReportNode;
 import com.example.idlereasonsproject.FBDatabase.ReportObject;
@@ -8,6 +10,35 @@ import java.util.Map;
 import java.util.Objects;
 
 public class machineAnalysis {
+//Basic machine analysis
+    //isMachineIdle only searches the full report list, and also rely on getMachine returning a string and not a machine object
+    public static boolean isMachineIdle (MachineObject machine) {
+        Map<String, ReportObject> unresolvedReports = reportAnalysis.getUnresolvedReports(ReportNode.getReportMap());
+        for (Map.Entry<String, ReportObject> entry : unresolvedReports.entrySet()) {
+            ReportObject value = entry.getValue();
+            if (Objects.equals(value.getMachine(), machine.getName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+    //machinesIdleReport only searches the full report list, if there's multiple reports with a machine it'll return the first
+    //Might be redundant too I think Jackson added something so machineObjects have report objects attached to them
+    public static ReportObject machinesIdleReport (MachineObject machine) {
+        if (!isMachineIdle(machine)) {
+            Log.w("machinesIdleReport", machine.getName() + "isn't idle");
+            return new ReportObject("Machine isn't idle", "Machine isn't idle", "Machine isn't idle", "Machine isn't idle");
+        }
+        Map<String, ReportObject> unresolvedReports = reportAnalysis.getUnresolvedReports(ReportNode.getReportMap());
+        for (Map.Entry<String, ReportObject> entry : unresolvedReports.entrySet()) {
+            ReportObject value = entry.getValue();
+            if (Objects.equals(value.getMachine(), machine.getName())) {
+                return value;
+            }
+        }
+        Log.e("machinesIdleReport", machine.getName() + " was said to be idle by isMachineIdle, but machinesIdleReport found no report");
+        return new ReportObject("error", "error", "error", "error");
+    }
 //For Filtering
     //No checks for inconsistent capitalization
     public static Map<String, MachineObject> getMapOfMachinesOfType(String type, Map<String, MachineObject> map) {
@@ -20,17 +51,6 @@ public class machineAnalysis {
             }
         }
         return returnedMap;
-    }
-    //isMachineIdle only searches the full report list, and also rely on getMachine returning a string and not a machine object
-    public static boolean isMachineIdle (MachineObject machine) {
-        Map<String, ReportObject> unresolvedReports = reportAnalysis.getUnresolvedReports(ReportNode.getReportMap());
-        for (Map.Entry<String, ReportObject> entry : unresolvedReports.entrySet()) {
-            ReportObject value = entry.getValue();
-            if (Objects.equals(value.getMachine(), machine.getName())) {
-                return true;
-            }
-        }
-        return false;
     }
     //Uses isMachineIdle and therefore works with the full report list
     public static Map<String, MachineObject> getMapOfIdleMachines(Map<String, MachineObject> map) {
