d8b6b06ef76e809a2d7cfe8b2b9334d77578c9d3
weatherspoonjackson <jack.weatherspoon7@gmail.com>
2024-05-09 15:19:43 -0500

    added polygons to maps

--- app/src/main/java/com/example/idlereasonsproject/MapsActivity.java
+++ app/src/main/java/com/example/idlereasonsproject/MapsActivity.java
@@ -11,13 +11,28 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.idlereasonsproject.databinding.ActivityMapsBinding;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+    GoogleMap.OnMarkerClickListener, GoogleMap.OnMarkerDragListener{
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+
+    private List<LatLng> polygonPoints = new ArrayList<>();
+    private LatLng endPoint;
+
+    private Polygon polygon;
+
+    private int markerDragged = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,16 +60,85 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        PolygonOptions polygonOptions = new PolygonOptions();
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(@NonNull LatLng latLng) {
                 Log.v("googleMaps", latLng.toString());
+
+                mMap.addMarker(new MarkerOptions()
+                        .position(latLng)
+                        .draggable(true));
+
+                if(polygonPoints.size() > 1)
+                {
+                    polygonPoints.add(polygonPoints.size()-1, latLng);
+                    polygon.setPoints(polygonPoints);
+                }
+                else
+                {
+                    //set the first and last points to close the loop of the polygon
+                    polygonPoints.add(latLng);
+                    polygonPoints.add(latLng);
+                    endPoint = latLng;
+                    polygonOptions.addAll(polygonPoints);
+
+                    polygon = mMap.addPolygon(polygonOptions);
+                }
+
+                Log.v("polygonPoints", polygonPoints.toString());
             }
         });
+
+        mMap.setOnMarkerClickListener(this);
+        mMap.setOnMarkerDragListener(this);
+    }
+
+    @Override
+    public boolean onMarkerClick(final Marker marker)
+    {
+        Log.v("onMarkerClick", marker.getPosition().toString());
+        return false;
+    }
+
+    @Override
+    public void onMarkerDrag(@NonNull Marker marker)
+    {
+        Log.v("onMarkerDrag", marker.getPosition().toString());
+        LatLng newPos = marker.getPosition();
+        Log.v("onMarkerDrag", String.valueOf(markerDragged));
+
+        if(markerDragged == 0)
+        {
+            polygonPoints.set(markerDragged, newPos);
+            polygonPoints.set(polygonPoints.size()-1, newPos);
+        }
+
+        polygon.setPoints(polygonPoints);
+    }
+
+    @Override
+    public void onMarkerDragEnd(@NonNull Marker marker)
+    {
+        Log.v("onMarkerDragEnd", marker.getPosition().toString());
+        markerDragged = -1;
+    }
+
+    @Override
+    public void onMarkerDragStart(@NonNull Marker marker)
+    {
+        LatLng pos = marker.getPosition();
+        Log.v("onMarkerDragStart", pos.toString());
+        Log.v("endpoint", endPoint.toString());
+        Log.v("compare", String.valueOf(pos.equals(endPoint)));
+        if(pos.equals(endPoint))
+        {
+            markerDragged = 0;
+            return;
+        }
     }
 }
