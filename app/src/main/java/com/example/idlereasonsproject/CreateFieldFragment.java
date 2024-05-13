ebdd8af1d8a98be34981ef454b0cbd332b78530c
weatherspoonjackson <jack.weatherspoon7@gmail.com>
2024-05-13 15:20:22 -0500

    added maps to CreateFieldFragment.java

--- app/src/main/java/com/example/idlereasonsproject/CreateFieldFragment.java
+++ app/src/main/java/com/example/idlereasonsproject/CreateFieldFragment.java
@@ -18,13 +18,25 @@ import com.example.idlereasonsproject.FBDatabase.FieldNode;
 import com.example.idlereasonsproject.FBDatabase.FieldObject;
 import com.example.idlereasonsproject.databinding.FragmentCreateFieldBinding;
 import com.example.idlereasonsproject.iface.DrawerLocker;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class CreateFieldFragment extends Fragment
+public class CreateFieldFragment extends Fragment implements OnMapReadyCallback
 {
     private FragmentCreateFieldBinding binding;
     private FieldNode fieldNode;
 
     private EditText fieldNameInput;
+
+    private MapView mapView;
+    private GoogleMap map;
 
     @Override
     public View onCreateView(
@@ -34,6 +46,10 @@ public class CreateFieldFragment extends Fragment
         binding = FragmentCreateFieldBinding.inflate(inflater, container, false);
         fieldNode = Database.fieldNode;
         fieldNameInput = binding.fieldNameTextInputLayout.getEditText();
+
+        mapView = binding.createFieldMap;
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
 
         return binding.getRoot();
     }
@@ -53,6 +69,30 @@ public class CreateFieldFragment extends Fragment
                         .navigate(R.id.action_CreateField_to_FieldList); //later change to go to that new field details screen
             }
         });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap)
+    {
+        map = googleMap;
+        map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        //test for now
+        LatLng farm = new LatLng(41.722370, -90.595100);
+        map.addMarker(new MarkerOptions().position(farm));
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(farm,10));
+
+
+        map.getUiSettings().setMyLocationButtonEnabled(false);
+        map.getUiSettings().setScrollGesturesEnabled(true);
+        map.getUiSettings().setZoomControlsEnabled(true);
+    }
+
+    @Override
+    public void onResume()
+    {
+        mapView.onResume();
+        super.onResume();
     }
 
 
