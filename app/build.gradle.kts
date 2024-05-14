69122c7de19c5b37869e59b396d7238d915c2437
Jackson W <jack.weatherspoon7@gmail.com>
2024-05-14 06:49:32 -0500

    Added new stuff for polygons on map

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,7 +61,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("com.android.car.ui:car-ui-lib:2.6.0")
-    implementation("com.google.android.gms:play-services-maps:18.1.0")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/idlereasonsproject/MapsActivity.java
+++ app/src/main/java/com/example/idlereasonsproject/MapsActivity.java
@@ -16,10 +16,11 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.idlereasonsproject.databinding.ActivityMapsBinding;
 import com.google.android.gms.maps.model.Polygon;
 import com.google.android.gms.maps.model.PolygonOptions;
-import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
     GoogleMap.OnMarkerClickListener, GoogleMap.OnMarkerDragListener{
@@ -27,12 +28,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
 
-    private List<LatLng> polygonPoints = new ArrayList<>();
-    private LatLng endPoint;
+    //map to keep track of which marker goes with which polygon point
+    private final Map<Marker, Integer> markerMap = new HashMap<>();
+
+    //list to keep track of the polygon points
+    private final List<LatLng> polygonPoints = new ArrayList<>();
+
+    //marker that represents the start/end of the polygon
+    private Marker endPoint;
 
     private Polygon polygon;
 
     private int markerDragged = -1;
+    private boolean wasDragged = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,7 +65,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * installed Google Play services and returned to the app.
      */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
 
         PolygonOptions polygonOptions = new PolygonOptions();
@@ -68,15 +76,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(@NonNull LatLng latLng) {
-                Log.v("googleMaps", latLng.toString());
-
-                mMap.addMarker(new MarkerOptions()
+                //make new marker at click
+                Marker marker = mMap.addMarker(new MarkerOptions()
                         .position(latLng)
                         .draggable(true));
 
+
                 if(polygonPoints.size() > 1)
                 {
+                    //polygonPoints.size()-1
+                    //int index = setNewPoint(latLng);
+                    markerMap.put(marker, polygonPoints.size()-1);
                     polygonPoints.add(polygonPoints.size()-1, latLng);
+
+
                     polygon.setPoints(polygonPoints);
                 }
                 else
@@ -84,15 +97,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     //set the first and last points to close the loop of the polygon
                     polygonPoints.add(latLng);
                     polygonPoints.add(latLng);
-                    endPoint = latLng;
+                    markerMap.put(marker, 0);
+                    endPoint = marker;
+
                     polygonOptions.addAll(polygonPoints);
 
                     polygon = mMap.addPolygon(polygonOptions);
                 }
-
-                Log.v("polygonPoints", polygonPoints.toString());
-            }
-        });
+            }});
 
         mMap.setOnMarkerClickListener(this);
         mMap.setOnMarkerDragListener(this);
@@ -101,21 +113,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public boolean onMarkerClick(final Marker marker)
     {
-        Log.v("onMarkerClick", marker.getPosition().toString());
+        Log.v("onMarkerClick", marker.getPosition().toString() +"\n" + markerMap.get(marker));
         return false;
     }
 
     @Override
     public void onMarkerDrag(@NonNull Marker marker)
     {
-        Log.v("onMarkerDrag", marker.getPosition().toString());
+        wasDragged = true;
         LatLng newPos = marker.getPosition();
-        Log.v("onMarkerDrag", String.valueOf(markerDragged));
-
+        //if the marker is the start/end point move both points
         if(markerDragged == 0)
         {
             polygonPoints.set(markerDragged, newPos);
             polygonPoints.set(polygonPoints.size()-1, newPos);
+        }
+        else
+        {
+            polygonPoints.set(markerDragged, newPos);
         }
 
         polygon.setPoints(polygonPoints);
@@ -124,21 +139,72 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMarkerDragEnd(@NonNull Marker marker)
     {
-        Log.v("onMarkerDragEnd", marker.getPosition().toString());
+        if(!wasDragged)
+        {
+            marker.setPosition(polygonPoints.get(markerDragged));
+        }
+
+        //restart marker dragged once finished
         markerDragged = -1;
+        wasDragged = false;
     }
 
     @Override
     public void onMarkerDragStart(@NonNull Marker marker)
     {
-        LatLng pos = marker.getPosition();
-        Log.v("onMarkerDragStart", pos.toString());
-        Log.v("endpoint", endPoint.toString());
-        Log.v("compare", String.valueOf(pos.equals(endPoint)));
-        if(pos.equals(endPoint))
+        //get index of point from marker map
+        markerDragged = markerMap.get(marker);
+    }
+
+    public int setNewPoint(LatLng point)
+    {
+        if(polygonPoints.size() <= 3)
         {
-            markerDragged = 0;
-            return;
+            return 1;
         }
+
+        int smallestIndex = 0;
+        double smallestDist = findDistance(point, polygonPoints.get(0));
+
+        int secondSmallestIndex = 1;
+        double secondSmallestDist = findDistance(point, polygonPoints.get(1));
+
+        for(int i=0; i<polygonPoints.size();i++)
+        {
+            double dist = findDistance(point, polygonPoints.get(i));
+
+            if(dist < smallestDist)
+            {
+                smallestIndex = i;
+                smallestDist = dist;
+            }
+            else if(dist < secondSmallestDist && dist > smallestDist && i != smallestIndex)
+            {
+                secondSmallestIndex = i;
+                secondSmallestDist = dist;
+            }
+        }
+
+        Log.v("index", smallestIndex + " " + smallestDist + "\n" + secondSmallestIndex + " " + secondSmallestDist +"\n" + polygonPoints.toString());
+        return Math.min(smallestIndex, secondSmallestIndex);
+    }
+
+    public double findDistance(LatLng pnt1, LatLng pnt2)
+    {
+        double degsToRads = Math.PI/180;
+        double earthRadius = 6371; //average radius in km
+
+        //convert to radians
+        double lon1 = pnt1.longitude * degsToRads;
+        double lon2 = pnt2.longitude * degsToRads;
+        double lat1 = pnt1.latitude * degsToRads;
+        double lat2 = pnt2.latitude * degsToRads;
+
+        //I split the equation to find distance using
+        //lat/lon into 3 parts
+        double a = Math.sin(lat1) * Math.sin(lat2);
+        double b = Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon2 - lon1);
+
+        return Math.acos(a+b) * earthRadius;
     }
 }
